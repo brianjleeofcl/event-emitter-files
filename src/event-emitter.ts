e91a3c9e17e8aa7a5700f9619b7eb960050b3c82
Brian Lee <brianjleeofcl@gmail.com>
2017-05-22 11:49:13 -0700

    Clean up code

--- src/event-emitter.ts
+++ src/event-emitter.ts
@@ -1,7 +1,6 @@
 import { EventEmitter } from 'events';
 
 export const customEE = new EventEmitter();
-// customEE.on('ping', (num: number) => console.log('ping:', num));
 
 let count: number = 0;
 setInterval(() => {
@@ -9,11 +8,3 @@ setInterval(() => {
   count++;
   if (count > 10) process.exit(0)
 }, 2500)
-
-
-
-// import { createReadStream } from 'fs';
-// import { join } from 'path'
-
-// createReadStream(join(__dirname, 'fib-gen.js'), 'utf-8').emit('readstream')
-
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ const callback = function (num: number) {
   console.log(`From the imported module, ping #${num}`)
 }
 
-customEE.once('ping', () => console.log('Pinging started!')).on('ping', callback).on('ping', (num: number) => {
-  if (num > 5) {
-    customEE.removeListener('ping', callback);
-  }
+customEE.once('ping', () => console.log('Pinging started!'))
+  .on('ping', callback).on('ping', (num: number) => {
+    if (num > 5) {
+      customEE.removeListener('ping', callback);
+    }
 })
-
